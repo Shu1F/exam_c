8ef0d8e0209e7b18e43d2ad0aa6ea0a190ad6adf
Shuichi Fujiike <fs2031@icloud.com>
2026-07-18 12:45:27 +0900

    演習1-4

--- 1/演習/1.c
+++ 1/演習/1.c
@@ -41,22 +41,44 @@
 
 // 演習3
 
-int main() {
-    int i;
+// int main() {
+//     int i;
 
+//     char s[1024];
+//     char t[1024];
+
+//     scanf("%s %s", s, t);
+
+//     for(i = 0; s[i] != '\0' && t[i] != '\0'; i++) {
+//         if (s[i] != t[i]){
+//             return 0;
+//         }
+//     }
+//     // どちらかが最終文字でない可能性を考慮（どちらかの文字列が長い可能性）
+//     if (s[i] == '\0' || t[i] == '\0') {
+//         return 0;
+//     }
+//     return 1;
+// }
+
+// 演習4
+
+int main(void) {
     char s[1024];
     char t[1024];
 
-    scanf("%s %s", s, t);
+    int i = 0;
+    int len = 0;
 
-    for(i = 0; s[i] != '\0' && t[i] != '\0'; i++) {
-        if (s[i] != t[i]){
-            return 0;
-        }
+    while(s[i] != '\0') {
+        i++;
+        len++;
     }
-    // どちらかが最終文字でない可能性を考慮（どちらかの文字列が長い可能性）
-    if (s[i] == '\0' || t[i] == '\0') {
-        return 0;
+
+    int j;
+
+    for(j = 0; t[j] != '\0'; j++) {
+        s[len + j] += t[j];
     }
-    return 1;
+    printf("%s", s);
 }

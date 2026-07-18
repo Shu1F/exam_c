948dddeac070c93f7397ed8beb5b14a1ead78790
Shuichi Fujiike <fs2031@icloud.com>
2026-07-18 12:28:13 +0900

    演習1-2

--- 1/演習/1.c
+++ 1/演習/1.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 
+// 演習１
+
+// int main(void) {
+//     char s[1024];
+//     int i = 0;
+//     int len = 0;
+//     scanf("%s", s);
+
+//     while(s[i] != '\0') {
+//         i++;
+//         len++;
+//     }
+//     printf("%d", len);
+// }
+
+
+// 演習２
+
 int main(void) {
     char s[1024];
-    int i = 0;
+    char t[1024];
+    int i;
     int len = 0;
+
     scanf("%s", s);
 
-    while(s[i] != '\0') {
+    i = 0;
+    while(s[i] != '\0'){
         i++;
         len++;
     }
-    printf("%d", len);
+
+    for(i = 0; i < len; i++) {
+        t[i] = s[i];
+    }
+
+    printf("%s", t);
 }

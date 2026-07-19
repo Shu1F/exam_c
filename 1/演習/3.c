6280a1ee387c23f13dc0ba00d62956cb292d23d3
Shuichi Fujiike <fs2031@icloud.com>
2026-07-19 19:14:15 +0900

    chore

--- 1/演習/3.c
+++ 1/演習/3.c
@@ -1,37 +1,37 @@
-# include<stdio.h>
-# include<stdlib.h>
+// # include<stdio.h>
+// # include<stdlib.h>
 
-int main () {
-    int n, x;
-    int *a;
-    int i;
-    int ans = -1;
+// int main () {
+//     int n, x;
+//     int *a;
+//     int i;
+//     int ans = -1;
 
-    scanf("%d %d", &n, &x);
+//     scanf("%d %d", &n, &x);
 
-    a = (int *)malloc(sizeof(int) * n);
+//     a = (int *)malloc(sizeof(int) * n);
 
-    if(a == NULL) {
-        printf("ERROR\n");
-        return 0;
-    }
+//     if(a == NULL) {
+//         printf("ERROR\n");
+//         return 0;
+//     }
 
-    for (i=0; i<n; i++) {
-        scanf("%d", &a[i]);
-    }
+//     for (i=0; i<n; i++) {
+//         scanf("%d", &a[i]);
+//     }
 
-    for(i=0; i<n; i++) {
-        if(a[i] == x) {
-            ans = i;
-            break;
-        }
-    }
-    if (ans != -1) {
-        printf("a[%d] = %d", ans, a[ans]);
-    } else {
-        printf("not found");
-    }
+//     for(i=0; i<n; i++) {
+//         if(a[i] == x) {
+//             ans = i;
+//             break;
+//         }
+//     }
+//     if (ans != -1) {
+//         printf("a[%d] = %d", ans, a[ans]);
+//     } else {
+//         printf("not found");
+//     }
 
-    free(a);
-    return 0;
-}
+//     free(a);
+//     return 0;
+// }
